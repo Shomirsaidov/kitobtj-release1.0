1c66b47f9bc278da09b53d2080f10a323daacde7
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2024-01-06 05:04:34 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BasketView from '../views/BasketView.vue'
 
@@ -75,7 +75,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
